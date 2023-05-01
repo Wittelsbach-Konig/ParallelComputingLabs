6cbf668e8d25903fecf3649f3ec5509b69fd55ba
Wittelsbach-Konig <kiryuschin.vitalij@yandex.ru>
2023-05-01 15:43:19 +0300

    Add pragma section

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -6,14 +6,21 @@
 #include <omp.h>
 
 
+#define _OPENMP
+#define CHUNK 1
+#define SCHEDULE guided
+
+
 int main(int argc, char* argv[]) {
     int i, N;
     struct timeval T1, T2;
     long delta_ms;
     N = atoi(argv[1]); /* N равен первому параметру командной строки */
-    //omp_set_dynamic(0);
-    //const int THREADS = atoi(argv[2]);
-    //omp_set_num_threads(THREADS);
+    #if defined(_OPENMP)
+        omp_set_dynamic(0);
+        const int THREADS = atoi(argv[2]);
+        omp_set_num_threads(THREADS);
+    #endif
     gettimeofday(&T1, NULL); /* запомнить текущее время T1 */
     int N2 = N/2; /* N2 равен N/2*/
     double* restrict M1 = (double*) malloc(N * sizeof(double)); 
@@ -30,6 +37,7 @@ int main(int argc, char* argv[]) {
     for (i=0; i < 100; ++i) { /* 100 экспериментов */
         /* инициализировать начальное значение ГСЧ */
         seed = i;
+        X = 0.0;
         /* Заполнить массив исходных данных размером N */
         // GENERATE
         for(j=0; j < N; ++j) {
@@ -39,62 +47,91 @@ int main(int argc, char* argv[]) {
             M2[k]=((double)rand_r(seedp1) / (RAND_MAX)) * (max_2 - max) + max;
         }
         /*----------------------------------------------------------------------*/
-        // MAP
-        #pragma omp parallel for default(none) shared(N, M1) private(j)
-        for (j = 0; j < N; ++j) {
-            M1[j] = exp(sqrt(M1[j]));
-        }
-        #pragma omp parallel for default(none) shared(N2, M2, M2_old) private(k)
-        for (k = 0; k < N2; ++k) {
-            M2_old[k] = M2[k];
-        }
-        #pragma omp parallel for default(none) shared(N2, M2, M2_old) private(k)
-        for(k = 0; k < N2; ++k){
-            if(k > 0) {
+        #pragma omp parallel default(none) shared(N, N2, M1, M2, M2_old, key, X) private(k)
+        {
+            // MAP
+            #if defined(CHUNK) && defined(SCHEDULE)
+            #pragma omp for schedule(SCHEDULE, CHUNK) nowait
+            #else
+            #pragma omp for nowait
+            #endif
+            for (k = 0; k < N; ++k) {
+                M1[k] = exp(sqrt(M1[k]));
+            }
+            #if defined(CHUNK) && defined(SCHEDULE)
+            #pragma omp for schedule(SCHEDULE, CHUNK) nowait
+            #else
+            #pragma omp for nowait
+            #endif
+            for (k = 0; k < N2; ++k) {
+                M2_old[k] = M2[k];
+            }
+            #if defined(CHUNK) && defined(SCHEDULE)
+            #pragma omp for schedule(SCHEDULE, CHUNK)
+            #else
+            #pragma omp for
+            #endif
+            for(k = 1; k < N2; ++k) {
                 M2[k] = M2[k] + M2_old[k-1];
             }
-            M2[k] = log(fabs(tan(M2[k])));
-        }
-        // MERGE
-        #pragma omp parallel for default(none) shared(N2, M2, M1) private(k)
-        for(k=0; k < N2; ++k) {
-            M2[k]= M1[k] * M2[k];
-        }
+            #if defined(CHUNK) && defined(SCHEDULE)
+            #pragma omp for schedule(SCHEDULE, CHUNK)
+            #else
+            #pragma omp for
+            #endif
+            for(k = 0; k < N2; ++k) {
+                M2[k] = log(fabs(tan(M2[k])));
+            }
+            // MERGE
+            #if defined(CHUNK) && defined(SCHEDULE)
+            #pragma omp for schedule(SCHEDULE, CHUNK)
+            #else
+            #pragma omp for
+            #endif
+            for(k=0; k < N2; ++k) {
+                M2[k]= M1[k] * M2[k];
+            }
+            /*----------------------------------------------------------------------*/
+            // SORT
+            /*
+            for (k = 0; k < (N2-1); ++k) {
+                min_s = k;
+                for (j = k + 1; j < N2; ++j) {
+                    if (M2[min_s] > M2[j]) {
+                        min_s = j;
+                    }
+                }
+                key = M2[min_s];
+                for(z = min_s; z > k; --z) {
+                    M2[z] = M2[z-1];
+                }
+                M2[k] = key;
+            }
+            */
         /*----------------------------------------------------------------------*/
-        // SORT
-        /*
-        for (k = 0; k < (N2-1); ++k) {
-            min_s = k;
-            for (j = k + 1; j < N2; ++j) {
-                if (M2[min_s] > M2[j]) {
-                    min_s = j;
+            // REDUCE            
+            #pragma omp single
+            key = M2[0];
+            #pragma omp single
+            for (k = 1; k < N2; ++k) {
+                if(M2[k] != 0) {
+                    if (key == 0 || M2[k] < key) {
+                        key = M2[k];
+                    }
                 }
             }
-            key = M2[min_s];
-            for(z = min_s; z > k; --z) {
-                M2[z] = M2[z-1];
-            }
-            M2[k] = key;
-        }
-        */
-       /*----------------------------------------------------------------------*/
-        // REDUCE
-        X = 0.0;
-        key = M2[0];
-        for (k = 1; k < N2; ++k) {
-            if(M2[k] != 0) {
-                if (key == 0 || M2[k] < key) {
-                    key = M2[k];
+            #if defined(CHUNK) && defined(SCHEDULE)
+            #pragma omp for reduction(+ : X) schedule(SCHEDULE, CHUNK)
+            #else
+            #pragma omp for reduction(+ : X)
+            #endif
+            for(k = 0; k < N2; k++) {
+                if (((int)(M2[k] / key) % 2) == 0) {
+                    X += sin(M2[k]);
                 }
             }
+            /*----------------------------------------------------------------------*/
         }
-        #pragma omp parallel for default(none) shared(N2, M2, key) private(k) reduction(+ : X)
-        for(k = 0; k < N2; k++) {
-            if (((int)(M2[k] / key) % 2) == 0) {
-                X += sin(M2[k]);
-            }
-        }
-        /*----------------------------------------------------------------------*/
         //printf("X = %f", X);
         //printf("\n\n");
         /* Решить поставленную задачу, заполнить массив с результатами*/
